f18196d5b315043d0643b3f2f84b747bac3ec234
NelsonRodMar <contact@nelsonrodmar.fr>
2025-03-31 12:43:19 +0100

    try to allow all source

--- next.config.js
+++ next.config.js
@@ -42,7 +42,7 @@ module.exports = withBundleAnalyzer({
     return [
       {
         // matching all API routes
-        source: '/api/:path*',
+        source: '*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
